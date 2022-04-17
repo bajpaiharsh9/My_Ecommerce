7508d91f94de9940a147cbb2b7dfd60125df2269
HarshBajpai <bajpaiharsh9@gmail.com>
2022-04-17 14:09:09 +0530

    ecommerce

---
+++ script.js
@@ -0,0 +1,15 @@
+const bar = document.querySelector('#bar');
+const navbar = document.querySelector('#navbar');
+const close = document.getElementById('close')
+
+if(bar){
+    bar.addEventListener("click", ()=>{
+        navbar.classList.add('active');
+    })
+}
+
+if(close){
+    close.addEventListener("click", ()=>{
+        navbar.classList.remove('active');
+    })
+}
